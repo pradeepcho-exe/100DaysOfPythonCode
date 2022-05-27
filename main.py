a612f4aab7643207187fa3f0fa8fa218af9ff31b
Pradeep Kumar Kumar <102564484+pradeepcho-exe@users.noreply.github.com>
2022-05-27 08:30:18 +0000

    Leap Year Program

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 # 🚨 Don't change the code below 👇
-height = float(input("enter your height in m: "))
-weight = float(input("enter your weight in kg: "))
+year = int(input("Which year do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-bmi = round(weight / (height ** 2))
-
-if bmi < 18.5:
-  print(f"Your BMI is {bmi}, you are underweight.")
-elif bmi<25:
-  print(f"Your BMI is {bmi}, you have a normal weight.")
-elif  bmi<30:
-  print(f"Your BMI is {bmi}, you are slightly overweight.")
-elif  bmi<35:
-  print(f"Your BMI is {bmi}, you are obese.")
+if year%4 == 0:
+  if year%100 ==0:
+    if year%400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
 else:
-  print(f"Your BMI is {bmi}, you are clinically obese.")
+  print("Not leap year.")
